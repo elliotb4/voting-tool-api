19685af8d1fc22de5013f8001f7d4499c1b5d625
elliotb4 <elliot.broxton@gmail.com>
2022-09-15 20:09:04 +0100

    jest installed

---
+++ test/api.test.js
@@ -0,0 +1,11 @@
+describe("Voting Algorithms", () => {
+  test.todo("borda works");
+
+  test.todo("condorcet works");
+
+  test.todo("irv works");
+
+  test.todo("plurality works");
+
+  test.todo("stv works");
+});
